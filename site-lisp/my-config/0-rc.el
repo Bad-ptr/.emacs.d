00f76bb8808bb806ae1a8c9920df38a457ba5f00
Constantin Kulikov <zxnotdead@gmail.com>
2017-01-16 13:47:23 +0300

    Remove isearch-delete-something.

--- site-lisp/my-config/0-rc.el
+++ site-lisp/my-config/0-rc.el
@@ -231,27 +231,6 @@ the syntax class ')'."
 
 (setq x-alt-keysym 'meta)
 
-;; isearch
-(defun isearch-delete-something ()
-  "Delete non-matching text or the last character."
-  ;; https://gist.github.com/johnmastro/508fb22a2b4e1ce754e0
-  (interactive)
-  (if (= 0 (length isearch-string))
-      (ding)
-    (setq isearch-string
-          (substring isearch-string
-                     0
-                     (or (isearch-fail-pos) (1- (length isearch-string)))))
-    (setq isearch-message
-          (mapconcat #'isearch-text-char-description isearch-string "")))
-  (if isearch-other-end (goto-char isearch-other-end))
-  (isearch-search)
-  (isearch-push-state)
-  (isearch-update))
-
-(define-key isearch-mode-map (kbd "<backspace>")
-  #'isearch-delete-something)
-
 
 ;; indent automatically
 ;;(global-set-key (kbd "RET") #'newline-and-indent)
